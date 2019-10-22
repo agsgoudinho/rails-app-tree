a6d20ff7364c83e51e744130c1493dcb94235921
agsgoudinho <aguinaldo@alunos.utfpr.edu.br>
2019-10-21 22:05:09 -0300

    add phones

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -69,6 +69,6 @@ class ContactsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def contact_params
-      params.require(:contact).permit(:name, :birthday)
+      params.require(:contact).permit(:name, :birthday, phones_attributes: [:number, :_destroy, :id])
     end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,4 @@
 class Contact < ApplicationRecord
   has_many :phones, dependent: :restrict_with_error
+  accepts_nested_attributes_for :phones, reject_if: lambda { |a| a[:number].blank? }, allow_destroy: true
 end
