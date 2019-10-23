ce1a2fcca2b90a51aabe626215797ebe8c0fc0ac
agsgoudinho <aguinaldo@alunos.utfpr.edu.br>
2019-10-22 22:32:26 -0300

    nested-model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,4 @@
 class Contact < ApplicationRecord
-  has_many :phones, dependent: :restrict_with_error
+  has_many :phones, dependent: :destroy
   accepts_nested_attributes_for :phones, reject_if: lambda { |a| a[:number].blank? }, allow_destroy: true
 end
