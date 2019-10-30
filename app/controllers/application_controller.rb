229c1fa90cb3f6508e5359133a26589e06be9f9b
agsgoudinho <aguinaldo@alunos.utfpr.edu.br>
2019-10-29 21:29:35 -0300

    Generated users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
+  before_action :authenticate_user!
   def set_locale
     session[:locale] = params[:locale] || session[:locale] || I18n.default_locale
     I18n.locale = session[:locale]
